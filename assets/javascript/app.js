a244e8b16f41e7a800fd2286f7d15634768c3160
rdrachenberg <31778670+rdrachenberg@users.noreply.github.com>
2017-12-27 14:55:13 -0500

    working app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,81 +1,101 @@
 $(document).ready(function(){
-
 // global variables to populated buttons
   var animals = ["Bat", "Cat", "Bear", "Tiger", "Dog", "Monkey", 
-  "Snake", "Cougar", "Fish", "Goat", "Flying Squirrel", "Grasshopper", 
-  "Shark", "Giraffe",];
-  
-  var btn = $("<button>", + addButtons());
+  "Snake", "Cougar", "Fish", "Flying Squirrel", "Grasshopper", 
+  "Shark", "Giraffe"];
 
+  var overflow = [];
+  
+
+  var textInput = overflow;
+  // creates a variable btn and defines all buttons as that var  
+  var btn = $("<button>");
+  // creates variable s and defines input as that var
   var s = $("<input>");
   
 // Function to Display buttons dynamically from animals array
       function addButtons() {
-//*************NEEDS ATTENTION LINE 16 ***********************
+//*************NEEDS ATTENTION LINE 17 ***********************
         // Deleting the animals prior to adding new animals
-        $("#animals-view").empty();
-        $("#animals-view").add(animals[i]++);
+        
+        // $("#animals-view").empty();
+        
+        // $("#animals-view").add(animals[i]++);
         // Looping through the array of animals
         for (var i = 0; i < animals.length; i++) {
-          // Dynamicaly generate buttons for each animal listed in array
+          // Dynamicaly generate buttons for each string listed in array
           var z = $("<button>");
-          // Adding z class of animal to our button
+          // Adding class of animals to our button
           z.addClass("animals");
-          // Adding z data-attribute
+          // Adding data-search attribute and looking at each string in animals variable
           z.attr("data-search", animals[i]);
           // Providing the initial button text
           z.text(animals[i]);
           // Adding the button to the animals-view div
           $("#animals-view").append(z);
         }
-        // button on click listener function for AJAX Query 
-        $('button').on('click', function(){
+        giphyCall();
+      
+      };
+      addButtons();
+
+    function giphyCall() {
+       // button on click listener function for AJAX Query 
+      $('button').on('click', function(){
         //declares v variable to look in the data search area
-        var v = $(this).data('search');
+      var v = $(this).data('search');
         // holds giphy API query variable
-        var queryURL = ("https://api.giphy.com/v1/gifs/search?q=("+ v +")&api_key=gnPcnf9Mt3R3VMryknoAcv9HUzbbpqZT&limit=12");
-        // Executes the AJAX call
-          $.ajax({
-          url: queryURL,
-          method: 'GET'
-          }).done(function(response) {
-            // loop through the length of the AJAX response 
-            for(i=0; i < response.data.length; i++){
+      var queryURL = ("https://api.giphy.com/v1/gifs/search?q=("+ v +")&api_key=gnPcnf9Mt3R3VMryknoAcv9HUzbbpqZT&limit=12");
+      // Executes the AJAX call
+        $.ajax({
+        url: queryURL,
+        method: 'GET'
+        }).done(function(response) {
+          // loop through the length of the AJAX response 
+          for(i = 0; i < response.data.length; i++){
               
-              var frustratedDiv = $('<div>');
+          var secondDiv = $('<div>');
                       
-              var p = $('<p>').text("Rating: "+response.data[i].rating);
+          var p = $('<p>').text("Rating: "+response.data[i].rating);
                       
-              var frustratedImage = $('<img>');
+          var frustratedImage = $('<img>');
                       
-              frustratedImage.attr('src',response.data[i].images.fixed_height.url);
+          frustratedImage.attr('src',response.data[i].images.fixed_height.url);
                       
-              frustratedDiv.prepend(p);
+          secondDiv.prepend(p);
                       
-              frustratedDiv.prepend(frustratedImage);
+          secondDiv.prepend(frustratedImage);
                       
-              $('#gifsLoadHere').prepend(frustratedDiv);
-               console.log(response);
-            }
-          });
+          $('#gifsLoadHere').prepend(secondDiv);
+          }
         });
-      }
+      });
+    };
       // Pushes animalInput to Animals array and calls addButtons functions
-    $("#animalInput").on("change", function(event) {
+    $("#animalButton").on("click", function(event) {
         
         event.preventDefault();
 
-        var animalInput = $("#animalInput").val();
+        $("#animals-view2Buttons").empty();
+
+        var animalInput = $("#animalInput").val().trim();
       
-        animals.push(animalInput);
+        overflow.push(animalInput);
 
-        addButtons();
+        for (var i = 0; i < overflow.length; i++) {
+          // Dynamicaly generate buttons for each string listed in array
+          var t = $("<button>" + s);
+          // Adding class of animals to our button
+          t.addClass("overflow");
+          // Adding data-search attribute and looking at each string in overflow variable
+          t.attr("data-search", overflow[i]);
+          // Providing the initial button text
+          t.text(overflow[i]);
+          // Adding the button to the view2Buttons div
+          $("#animals-view2Buttons").append(t);
+        }
+        giphyCall();
 
-    });
-      // API Call when button is clicked
-    // $(document).on('click' , addButtons); 
-      
-      addButtons();
-
+    }); //ends on input "change" funciton 
 }); //end of docuemnt ready function
 
